6fcc8b2a3748f00da9af19202eb73c7900caf9d9
Sigo van der Linde <sigovdlinde@gmail.com>
2025-06-07 11:43:32 +0200

    Add snorlax.app domain configuration

--- arbius_gallery/settings.py
+++ arbius_gallery/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0,snorlax.app,www.snorlax.app', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
