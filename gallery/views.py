f1eaf1f79675efcd7dfc24f34bb59c0d79f5eccb
Sigo van der Linde <sigovdlinde@gmail.com>
2025-06-08 12:59:03 +0200

    Fix model filtering with correct Heroku database model IDs

--- gallery/views.py
+++ gallery/views.py
@@ -38,9 +38,9 @@ def get_image_models_queryset():
     ).exclude(
         model_id__startswith='0x000000'  # Filter out null/empty model IDs
     ).exclude(
-        model_id='0x89c39001e3b23d209b5c97bf881ed427aa6c54b5af2b3bc4b0b63e5b0c66e4bf6'  # Filter out text model
+        model_id='0x89c39001e3b23d2092bd998b62f07b523d23deb55e1627048b4ed47a4a38d5cc'  # Filter out text model (2564 images)
     ).exclude(
-        model_id='0x6cb3eed9fe3f32da1a23e923a61b5f0b43c5c66b16b6c4b99c81a0bf32b0ac0f'  # Filter out text model
+        model_id='0x6cb3eed9fe3f32da1910825b98bd49d537912c99410e7a35f30add137fd3b64c'  # Filter out text model (36 images)
     )
 
 
